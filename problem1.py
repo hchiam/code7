3d3c57854c9f3eabf83153d75309fa969b0b8020
hchiam <hchiam@gmail.com>
2016-06-19 20:03:25 -0400

    Starting with the actual code

--- problem1.py
+++ problem1.py
@@ -1,1 +1,14 @@
-print("test")
+# python code for problem 1
+
+with open("problem1.csv") as file:
+    for line in file:
+        print line
+        splitLine = line.split(",")
+        print splitLine
+        state = splitLine[0]
+        city = splitLine[1]
+        schoolGrade = splitLine[2]
+        number = splitLine[3]
+        print number
+        number = number.replace("K","")
+        print number
